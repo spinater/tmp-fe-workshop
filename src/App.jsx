ce34f213954b52fc02780036c7a0e88e675c5a3b
Adison Pianchob <K0962289>
2025-08-27 11:32:49 +0700

    x

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-root" style={{display: 'flex', height: '100vh', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
+      <h1>Hello World</h1>
+      <p>Welcome to Vite + React</p>
+    </div>
+  )
+}
+
+export default App
